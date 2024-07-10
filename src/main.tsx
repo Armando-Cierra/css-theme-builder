9edd720635e85855f581bc44953bdf8ace26b96b
Armando-Cierra <armando_cierra@outlook.com>
2024-07-10 11:47:38 -0600

    🚀 improving custom project settings to use vite-electron template for better support and documentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import '@/i18n/config.ts';
 import '@/styles/global.scss';
@@ -8,8 +8,8 @@ import '@/styles/variables.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
